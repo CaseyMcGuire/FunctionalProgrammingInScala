37983ced85c69d6691b78948882cc8b13ac0f6c6
Casey McGuire <casey.jay.mcguire@gmail.com>
2015-08-23 17:39:24 -0700

    Finished problem 3.25

--- ch3/Tree.scala
+++ ch3/Tree.scala
@@ -8,3 +8,15 @@
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+
+  //Exercise 3.25
+  //Write a function size that counts the number of nodes (leaves and branches) in a tree.
+  def size[A](tree: Tree[A]): Int = 
+    tree match {
+      case Branch(x,y) => size(x) + size(y)
+      case Leaf(_) => 1
+    }
+
+}
