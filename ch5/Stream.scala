de3bd77ddfa182915542ef6ce49c988bfa33505e
Casey McGuire <casey.jay.mcguire@gmail.com>
2015-12-25 21:07:03 -0800

    Did a couple more exercises

--- ch5/Stream.scala
+++ ch5/Stream.scala
@@ -143,7 +143,37 @@ sealed trait Stream[+A]{
   //Exercise 5.7
   //Implement map, filter, append, and flatMap using foldRight. The append method shoudl be
   //non-strict in its argument.
-  //  def map
+  def map[B](f: A => B): Stream[B] = 
+    foldRight(empty[B])((h, t) => cons(f(h), t))
+
+  def filter(f: A => Boolean): Stream[A] = 
+    foldRight[Stream[A]](empty)((h, t) => if(f(h)) cons(h, t) else t)
+
+  def append[B >: A](other: => Stream[B]): Stream[B] = 
+    foldRight(other)((h,t) => cons(h,t))
+
+
+  //Note that these implementations are incremental -- They don't fully generate their answers. It's
+  //not until some other computation looks at the elements of the resulting Stream that the 
+  //computation to generate that Stream actually takes place -- and then it will do just enough
+  //work to generate the requested elements. 
+
+  //Since we don't fully instantiate intermediate streams, it's exactly as if we had interleaved
+  //the logic using a special-purpose loop. For this reason, people sometimes describe streams
+  //as "first-class loops" whose logic can be combined using higher-order functions.
+
+  //Since intermediate streams aren't instantiated, it's easy to reuse existing combinators in
+  //novel ways without having to worry that we're doing more processing of the stream than 
+  //necessary.
+
+  //The incrememtal nature of stream transformation also has important consequences for memory usage.
+  //Because intermediate streams aren't generated, a transformation of the stream requires only
+  //enough working memory to store and transform the current element.
+
+  def find(p: A => Boolean): Option[A] = 
+    filter(p).headOption
+
+
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
@@ -165,23 +195,89 @@ object Stream {
   def apply[A](as: A*): Stream[A] = 
     if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 
+  //Infinite streams and corecursion
 
+  val ones: Stream[Int] = Stream.cons(1, ones)
 
+  //Exercise 5.8
+  //Generalize ones slightly to the function constant, which returns an infinite Stream of a given
+  //value
+  def constant[A](a: A): Stream[A] = Stream.cons(a, constant(a))
+
+  //Exercise 5.9
+  //Write a function that generates an infinite stream of integers, starting from n, then n + 1,
+  //n + 2, and so on.
+  def from(n: Int): Stream[Int] = Stream.cons(n, from(n + 1))
+
+  //Exercise 5.10
+  //Write a function fibs that generates the infinite stream of Fibonacci numbers: 0,1,1,2,3,5,8,
+  //and so on.
+  def fibs: Stream[Int] = {
+    def fibs(first: Int, second: Int): Stream[Int] = 
+      Stream.cons(first + second , fibs(second, (first + second)))
+    Stream.cons(0, Stream.cons(1, fibs(0,1)))
+  }
+
+  //Exercise 5.11
+  //Write a more general stream-building function called unfold. It takes an initial state, and a 
+  //function for producing both the next state and the next value in the generated stream.
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = {
+    f(z) match {
+      case Some((h, s)) => cons(h, unfold(s)(f))
+      case None => empty
+    }
+  }
+
+  //The unfold function is an example of what's sometimes called a corecursive function. Whereas a 
+  //recursive function consumes data, a corecursive function produces data. And whereas recursive
+  //functions terminate by recursing on smaller inputs, corecursive functions need not terminate
+  //so long as they remain productive, which just means that we can always evaluate more of the
+  //result in a finite amount of time. Corecursion is also sometimes called guarded recursion, and
+  //productivity is also sometimes called cotermination.
+
+  //Exercise 5.12
+  //Write fibs, from, constant, and ones in terms of unfold
+  def fibs_2: Stream[Int] =
+    unfold((0,1))((num: Tuple2[Int, Int]) => Some((num._1, (num._2, num._1 + num._2))))
+
+  def from_2(n: Int): Stream[Int] = 
+    unfold(n)((num: Int) => Some((num, num + 1)))
+
+  def constant_2[A](a: A): Stream[A] = 
+    unfold(a)((e: A) => Some((e, e)))
+
+  def ones_2: Stream[Int] = 
+    unfold(1)((Int) => Some((1, 1)))
 }
 
 object Test {
   def main(args: Array[String]) = {
     val str1 = Stream(1,2,3)
     val emptyStr = Stream()
+    val infiniteStr = constant(2)
+    val stepStr = from(1)
 
     runTest("forAll test less than 4", true,  str1.forAll(_ < 4))
     runTest("forAll test greater than 4", false, str1.forAll(_ > 4))
 
     runTest("Stream(1,2,3) headOption_2 is 1", 1, str1.headOption_2.get)
     runTest("Empty stream headOption_2 is None", None, emptyStr.headOption_2)
+
+    runTest("First four elements of infinite stream", infiniteStr.take(4).toList, List(2,2,2,2))
+    
+    runTest("First four elements of stepping stream", stepStr.take(4).toList, List(1,2,3,4))
+
+    runTest("First seven element of fibonacci stream", fibs.take(7).toList, List(0,1,1,2,3,5,8))
+
+
+    runTest("First seven elements of ones stream using unfold", ones_2.take(7).toList, List(1,1,1,1,1,1,1))
+
+    runTest("First seven elements of fibonacci stream using unfold", fibs_2.take(7).toList, List(0,1,1,2,3,5,8))
+
+    runTest("First seven elements of from stream (starting at 1) using unfold", from_2(1).take(7).toList, List(1,2,3,4,5,6,7))
   }
 
-  def runTest(testName: String, expected: Any, result: Any) {
+  def runTest(testName: String, result: Any, expected: Any) {
     if(result == expected) 
       println("PASS")
     else 
